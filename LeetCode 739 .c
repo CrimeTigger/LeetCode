16cf04a579c9f243e0c7191e05acacbdd9fec1ae
CrimeTigger <125776422+CrimeTigger@users.noreply.github.com>
2023-02-19 11:44:12 +0800

    Add files via upload

---
+++ LeetCode 739 .c
@@ -0,0 +1,24 @@
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+#include <string.h>
+int *dailyTemperatures(int *temperatures, int temperaturesSize, int *returnSize)
+{
+    int *res = (int *)malloc(sizeof(int) * temperaturesSize);
+    int stack[temperaturesSize + 1], top = 0;
+    memset(res, 0, temperaturesSize * sizeof(int));
+    *returnSize = temperaturesSize;
+    for (int i = 0; i < temperaturesSize; i++)
+    {
+        while (top > 0 && temperatures[i] > temperatures[stack[top - 1]])
+
+        {
+            res[stack[top - 1]] = i - stack[top - 1];
+            top--; /* code */
+        }
+        stack[top++] = i;
+    }
+    return res;
+}
