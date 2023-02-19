9ea87eddacc1303456b6ef24e874123a8e1c14e2
CrimeTigger <125776422+CrimeTigger@users.noreply.github.com>
2023-02-19 11:45:53 +0800

    Update LeetCode 739 .c

--- LeetCode 739 .c
+++ LeetCode 739 .c
@@ -8,7 +8,7 @@ int *dailyTemperatures(int *temperatures, int temperaturesSize, int *returnSize)
 {
     int *res = (int *)malloc(sizeof(int) * temperaturesSize);
     int stack[temperaturesSize + 1], top = 0;
-    memset(res, 0, temperaturesSize * sizeof(int));
+    memset(res, 0, temperaturesSize * sizeof(int)); //给res赋值0
     *returnSize = temperaturesSize;
     for (int i = 0; i < temperaturesSize; i++)
     {
