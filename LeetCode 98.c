cae411c750e3dc0970faad1d9635e618ef0eb92d
CrimeTigger <125776422+CrimeTigger@users.noreply.github.com>
2023-03-12 18:26:00 +0800

    Update LeetCode 98.c

--- LeetCode 98.c
+++ LeetCode 98.c
@@ -18,3 +18,39 @@ bool isValidBST(struct TreeNode *root)
     bool right = isValidBST(root->right);
     return left && right;
 }
+
+// 迭代
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isValidBST(struct TreeNode *root)
+{
+    if (!root)
+        return true;
+    struct TreeNode **stack = (struct TreeNode **)malloc(sizeof(struct TreeNode));
+    int top = -1;
+    struct TreeNode *cur = root;
+    struct TreeNode *pre = NULL;
+    while (root || top > -1)
+    {
+        if (root)
+        {
+            stack[++top] = cur; // 左
+            cur = cur->left;
+        }
+        else
+        {
+            cur = stack[top--]; // 中
+            if (pre && cur->val < pre->val)
+                return false;
+            pre = cur;
+            cur = cur->right; // 右
+        }
+    }
+    return true;
+}
